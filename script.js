ec428844edf02b7494357c755f4e2dced3b5b7c8
Eric Coe <coeeri001@warren.k12.in.us>
2024-09-23 08:40:04 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function printMessage(){
+   let element =  document.getElementById('print')
+    element.innerHTML ='Hello Word';
+   element.style.display = "none";
+}
+
+
+function display(){
+    let element =  document.getElementById('print')
+    element.style.display = "block";
+    element.style.color = "red";
+
+
+}
